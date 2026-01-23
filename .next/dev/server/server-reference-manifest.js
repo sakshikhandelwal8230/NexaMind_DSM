47c2416e48cc500ce43833551ad9e34597d79a03
vishal-parmar0 <vishal.parmar1032@gmail.com>
2026-01-24 00:36:19 +0530

    feat: add build and cache manifests for development environment
    - Introduced _buildManifest.js to manage page rewrites and sorted pages.
    - Created _clientMiddlewareManifest.json as an empty array for client middleware.
    - Added _ssgManifest.js to handle static site generation.
    - Implemented trace file to log development server performance metrics.
    - Defined cache-life types for Next.js to manage caching strategies.
    - Generated routes and validator types for improved type safety in app routing.

---
+++ .next/dev/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"bwYVJXUyIWwqJtfolgc4xMD7KaOhbGtfrqLvkoPZa8M=\"\n}"
---
+++ .next/dev/static/chunks/app_dashboard_user_page_tsx_c408a5a4._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_e4f78037._.js",
+  "static/chunks/node_modules_6f0dcf2b._.js"
+],
+    source: "dynamic"
+});
