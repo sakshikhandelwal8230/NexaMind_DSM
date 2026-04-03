d03508839dc6968093ed6bcfb8cf49cf0949ee06
Sakshi Khandelwal <sakshikhandelwal990@gmail.com>
2026-04-03 17:39:47 +0530

    update dependency

---
+++ .next/dev/static/chunks/app_dashboard_profile_page_tsx_063fdb01._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/_95342422._.js",
+  "static/chunks/node_modules_b0d6fb60._.js"
+],
+    source: "dynamic"
+});
